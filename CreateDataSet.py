aec8f66fec35897b21209595d8bee743c27ee455
Yulia <ts.yuli@gmail.com>
2020-12-31 10:29:53 +0200

    Yulia's last changes

--- CreateDataSet.py
+++ CreateDataSet.py
@@ -1,6 +1,10 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
+import os
 
+
+""" The purpose of this file:
+ class preparing a vocabilary and class preparing VQA dataset"""
 ### Hyper Parameters
 #int_size = 784
 #hid_size = 128
@@ -10,17 +14,45 @@ learning_rate = 0.1
 n_answers = 1000
 max_questions_len = 25 #30
 #num_classes = 10
+
+
 data_path = "/datashare"
 save_path = "./processed"
 
 class VQA_Vocab:
+    def __init__(self, vocab_file):
 
+        self.vocab = self.load_vocab(vocab_file)
+        self.vocab2idx = {vocab: idx for idx, vocab in enumerate(self.vocab)}
+        self.vocab_size = len(self.vocab)
+
+    def load_vocab(self, vocab_file):
+
+        with open(vocab_file) as f:
+            vocab = [v.strip() for v in f]
+
+        return vocab
+
+    def word2idx(self, vocab):
+
+        if vocab in self.vocab2idx:
+            return self.vocab2idx[vocab]
+        else:
+            return self.vocab2idx['<unk>']
+
+    def idx2word(self, idx):
+
+        return self.vocab[idx]
 
 
 class VQADataset(Dataset):
-    def __init__(self, data_path, max_nu_ans=10, max_q_len = max_questions_len, n_features: int = 1024, n_samples= n_answers):
+    def __init__(self, data_path, input_f_type, max_q_len = max_questions_len, n_features: int = 1024, n_samples= n_answers):
         # Set variables
-        self.path = path
+        #self.img_file_name='/datashare/%s/COCO_%s_%012d.jpg'%(input_f_type,input_f_type,image_id)
+        #path to images
+        self.img_input = torch.load(os.path.join(data_path,input_f_type))
+        #max questions len
+        self.max_questions_l = max_q_len
         # Load features
         self.features = self._get_features()
 
--- hw2_yul.py
+++ hw2_yul.py
@@ -7,6 +7,24 @@ from torch.utils.data import DataLoader
 
 if __name__ == '__main__':
 
+    ##starting with preprocessing of data, it should be made once, for creating txt and pickle files to be used by the model
+    ##data files locations +outputdir:
+    image_dir_train = "/datashare/train2014"
+    image_dir_val = "/datashare/val2014"
+    annotation_dir_train = "/datashare"
+    question_dir_train = "/datashare"
+    annotation_dir_val = "/datashare"
+    question_dir_val = "/datashare"
+    output_dir = "./processed"
+    #preprocessed data
+    proces_data = {}
+
+
+
+
+
+
+
     # Load dataset
     train_dataset = VQADataset(path=cfg['main']['paths']['train'])
     val_dataset = VQADataset(path=cfg['main']['paths']['validation'])
