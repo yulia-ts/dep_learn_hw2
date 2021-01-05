5d8d6d85023e24d2289d0f83fe7d27515bf6a98c
urielnusbaum <urieln@technion.ac.il>
2021-01-05 10:57:40 +0200

    unified all networks

--- models/MyNet.py
+++ models/MyNet.py
@@ -13,55 +13,6 @@ import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# input_size – The number of expected features in the input x
-# hidden_size – The number of features in the hidden state h
-# num_layers – Number of recurrent layers. E.g., setting num_layers=2 would mean stacking two RNNs together to form a stacked RNN, with the second RNN taking in outputs of the first RNN and computing the final results. Default: 1
-# nonlinearity – The non-linearity to use. Can be either 'tanh' or 'relu'. Default: 'tanh'
-# bias – If False, then the layer does not use bias weights b_ih and b_hh. Default: True
-# batch_first – If True, then the input and output tensors are provided as (batch, seq, feature). Default: False
-# dropout – If non-zero, introduces a Dropout layer on the outputs of each RNN layer except the last layer, with dropout probability equal to dropout. Default: 0
-# bidirectional – If True, becomes a bidirectional RNN. Default: False
-
-# Language Processing part
-EncoderRNN = nn.RNN(*args, **kwargs)
-DecoderRNN = nn.RNN(*args, **kwargs)
-# Attention decoder
-class AttnDecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
-        super(AttnDecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.dropout_p = dropout_p
-        self.max_length = max_length
-
-        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
-        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
-        self.dropout = nn.Dropout(self.dropout_p)
-        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
-        self.out = nn.Linear(self.hidden_size, self.output_size)
-
-    def forward(self, input, hidden, encoder_outputs):
-        embedded = self.embedding(input).view(1, 1, -1)
-        embedded = self.dropout(embedded)
-
-        attn_weights = F.softmax(
-            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
-        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
-                                 encoder_outputs.unsqueeze(0))
-
-        output = torch.cat((embedded[0], attn_applied[0]), 1)
-        output = self.attn_combine(output).unsqueeze(0)
-
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-
-        output = F.log_softmax(self.out(output[0]), dim=1)
-        return output, hidden, attn_weights
-
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
-
 # Image Processing part- A VGG implementation with dropouts, batchnorm, and relu activation
 # Images needs to be shaped into 224x224x3
 VGG_types = {
@@ -72,14 +23,14 @@ VGG_types = {
 }
 
 # if image is monochromatic change in_channels to 1, vgg_out is adjusted!!!! don't forget
-class MYVGG(nn.Module):
-    def __init__(self, in_channels=3, vgg_out=1024):
-        super(MYVGG, self).__init__()
-        self.in_channels = in_channels
-        # Define the convolution layers
+class VQANET(nn.Module):
+    def __init__(self, image_in_channels=3, vgg_out=1024, text_in_channels=1, terminal_in_channels=2, terminalnet_out=1000):
+        super(VQANET, self).__init__()
+        self.image_in_channels = image_in_channels
+        self.terminal_in_channels = terminal_in_channels        # Define the convolution layers
         self.conv_layers = self.create_conv_layers(VGG_types["VGG16"])  # choose your specific net here
-        # Define the fully connected part
-        self.fcs = nn.Sequential(
+        # Define the fully connected part of VGG
+        self.fcsvgg = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(),
             nn.Dropout(p=0.5),
@@ -88,11 +39,69 @@ class MYVGG(nn.Module):
             nn.Dropout(p=0.5),
             nn.Linear(4096, vgg_out),
         )
+        # Language Processing part
+        # Information can be found at- https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
+        EncoderRNN = nn.LSTM(*args, **kwargs)
+        DecoderRNN = nn.LSTM(*args, **kwargs)
 
-    def forward(self, x):
+        # Attention decoder
+        class AttnDecoderRNN(nn.Module):
+            def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+                super(AttnDecoderRNN, self).__init__()
+                self.hidden_size = hidden_size
+                self.output_size = output_size
+                self.dropout_p = dropout_p
+                self.max_length = max_length
+
+                self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+                self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+                self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+                self.dropout = nn.Dropout(self.dropout_p)
+                self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+                self.out = nn.Linear(self.hidden_size, self.output_size)
+
+            def forward(self, input, hidden, encoder_outputs):
+                embedded = self.embedding(input).view(1, 1, -1)
+                embedded = self.dropout(embedded)
+
+                attn_weights = F.softmax(
+                    self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+                attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                         encoder_outputs.unsqueeze(0))
+
+                output = torch.cat((embedded[0], attn_applied[0]), 1)
+                output = self.attn_combine(output).unsqueeze(0)
+
+                output = F.relu(output)
+                output, hidden = self.gru(output, hidden)
+
+                output = F.log_softmax(self.out(output[0]), dim=1)
+                return output, hidden, attn_weights
+
+            def initHidden(self):
+                return torch.zeros(1, 1, self.hidden_size, device=device)
+
+        #Define the terminal fully connected part
+        self.fcterminal = nn.Sequential(
+            nn.Linear(1024, terminalnet_out),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(terminalnet_out, terminalnet_out),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(terminalnet_out, terminalnet_out),
+            nn.Softmax(dim=terminalnet_out)
+        )
+
+    def forwardvgg(self, x):
         x = self.conv_layers(x)
         x = x.reshape(x.shape[0], -1)
-        x = self.fcs(x)
+        x = self.fcsvgg(x)
+        return x
+
+    def forwardterminal(self, x):
+        x = x[:, 0]*x[:, 1]  # This should be an elementwise multiplication
+        x = self.fcterminal(x)
         return x
 
     def create_conv_layers(self, architecture):
@@ -108,33 +117,3 @@ class MYVGG(nn.Module):
             elif x == "M":
                 layers += [nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))]
         return nn.Sequential(*layers)
-
-if __name__ == "__main__":
-    model = MYVGG(in_channels=3, vgg_out=1024).to(device)
-    print(model)
-    ## N = 3 (Mini batch size)
-    # x = torch.randn(3, 3, 224, 224).to(device)
-    # print(model(x).shape)
-
-
-# There are 2 1024 elements vectors as input to the terminal network
-class TerminalNet(nn.Module):
-    def __init__(self, in_channels=2, terminalnet_out=1000):
-        super(TerminalNet, self).__init__()
-        self.in_channels = in_channels
-        # Define the fully connected part
-        self.fcs = nn.Sequential(
-            nn.Linear(1024, terminalnet_out),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(terminalnet_out, terminalnet_out),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(terminalnet_out, terminalnet_out),
-            nn.Softmax(dim=terminalnet_out)
-        )
-
-    def forward(self, x):
-        u = x[:, 0]*x[:, 1]  # This should be an elementwise multiplication
-        u = self.fcs(u)
-        return u
